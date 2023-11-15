27863bd8f353b4f1bd1cf7a09d8f24af8eec519c
Aaryan Anand <144722275+AaryanAnand10@users.noreply.github.com>
2023-11-15 19:21:30 +0530

    Update Soil_Moisture.ino

--- Soil_Moisture.ino
+++ Soil_Moisture.ino
@@ -2,29 +2,38 @@ int sensor_pin = A0;
 
 void setup()
 {
+  // Initialize serial communication for debugging
   Serial.begin(9600);
+
+  // Set the sensor pin as INPUT
   pinMode(sensor_pin, INPUT);
 }
 
 void loop()
 {
+  // Read the analog value from the soil moisture sensor
   int sensor_data = analogRead(sensor_pin);
+
+  // Print the sensor data to the Serial Monitor
   Serial.print("Sensor_data:");
   Serial.print(sensor_data);
   Serial.print("\t | ");
-  
-  if(sensor_data > 950)
+
+  // Determine the moisture level based on the sensor reading
+  if (sensor_data > 950)
   {
     Serial.println("No moisture, Soil is dry");
   }
-  else if(sensor_data >= 400 && sensor_data <= 950)
+  else if (sensor_data >= 400 && sensor_data <= 950)
   {
     Serial.println("There is some moisture, Soil is medium");
   }
-  else if(sensor_data < 400)
+  else if (sensor_data < 400)
   {
     Serial.println("Soil is wet");
   }
 
+  // Delay for stability and readability in the Serial Monitor
   delay(100);
 }
+
